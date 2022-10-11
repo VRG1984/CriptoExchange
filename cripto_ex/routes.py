6dad327ed06aa117aafd09fdfb4779362314f922
Víctor R <vrg1984@gmail.com>
2022-10-11 17:36:19 +0200

    Creada carpeta 'cripto_ex', con un primer 'init', 'routes' e 'index.html', o lo que es lo mismo, los recursos mínimos necesarios para levantar el servidor, que ya rula

---
+++ cripto_ex/routes.py
@@ -0,0 +1,6 @@
+from cripto_ex import app
+from flask import render_template
+
+@app.route("/")
+def index():
+        return render_template("index.html", pageTitle="Todos", data=None)
---
+++ main.py
@@ -0,0 +1,1 @@
+from cripto_ex import app
